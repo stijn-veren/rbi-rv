d85ff7a4248a105da0add59dd49aa89edc9c9185
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-03 22:54:18 +0200

    Add fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState, useEffect, useCallback } from 'react'
 import { BiCalendar } from 'react-icons/bi'
 import AddAppointment from './components/AddAppointment'
 import AppointmentInfo from './components/AppointmentInfo'
 import Search from './components/Search'
-import appointmentList from './data.json'
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([])
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((data) => setAppointmentList(data))
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
