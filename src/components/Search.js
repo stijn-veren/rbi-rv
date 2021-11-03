e0f8a128f5a2dca449492d7f56956d6959770dc5
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-03 23:51:24 +0200

    Add sorting (with bug)

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import { BiSearch, BiCaretDown } from 'react-icons/bi'
 import DropDown from './DropDown'
 
-const Search = ({ query, onQueryChange }) => {
+const Search = ({
+  query,
+  onQueryChange,
+  sortBy,
+  onSortByChange,
+  orderBy,
+  onOrderByChange,
+}) => {
   let [toggleSort, setToggleSort] = useState(false)
-
   return (
     <div className="py-5">
       <div className="mt-1 relative rounded-md shadow-sm">
@@ -13,32 +19,37 @@ const Search = ({ query, onQueryChange }) => {
           <label htmlFor="query" className="sr-only" />
         </div>
         <input
-          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-8 leading-tight focus:outline-none"
           type="text"
           name="query"
           id="query"
           value={query}
-          placeholder="Search"
-          aria-label="Search"
           onChange={(event) => {
             onQueryChange(event.target.value)
           }}
+          className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300"
+          placeholder="Search"
         />
         <div className="absolute inset-y-0 right-0 flex items-center">
           <div>
             <button
               type="button"
+              onClick={() => {
+                setToggleSort(!toggleSort)
+              }}
               className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
               id="options-menu"
               aria-haspopup="true"
               aria-expanded="true"
-              onClick={() => {
-                setToggleSort(!toggleSort)
-              }}
             >
               Sort By <BiCaretDown className="ml-2" />
             </button>
-            <DropDown toggle={toggleSort} />
+            <DropDown
+              toggle={toggleSort}
+              sortBy={sortBy}
+              onSortByChange={(mySort) => onSortByChange(mySort)}
+              orderBy={orderBy}
+              onOrderByChange={(myOrder) => onOrderByChange(myOrder)}
+            />
           </div>
         </div>
       </div>
