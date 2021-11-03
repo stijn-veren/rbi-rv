0176fa41d2dfc915d69e9deaef99472fba695627
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-03 19:46:58 +0200

    Change icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BiArchive } from 'react-icons/bi'
+import { BiCalendar } from 'react-icons/bi'
 import AddAppointment from './components/AddAppointment'
 import Search from './components/Search'
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />
+        <BiCalendar className="inline-block text-red-400 align-top" />
         Your Appointments
       </h1>
       <AddAppointment />
